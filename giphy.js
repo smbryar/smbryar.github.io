fcbdf944b03d7edc2917eb5bd5d4456407b359b2
SaoBry <saoirse.bryar@softwire.com>
2021-08-23 16:24:52 +0100

    Enable searching for gifs

--- giphy.js
+++ giphy.js
@@ -1,17 +1,37 @@
 const apiKey = "9pz3h3ta7uGNedW8NC3Be57G3tic3RnQ";
 
-fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=cats&limit=12`)
-  .then((response) => response.json())
-  .then((json) => {
-    json.data
-      .map((gif) => gif.images.fixed_height.url)
-      .forEach((url) => {
-        const giphySection =
-          document.getElementsByClassName("giphy-container")[0];
-        const img = document.createElement("img");
-        img.src = url;
-        img.className = "giphy";
-        giphySection.appendChild(img);
-      });
-  })
-  .catch((error) => (document.body.appendChild = error));
+const fetchGifs = (e) => {
+  e.preventDefault();
+  const searchTerm = document.getElementById("gif-search-term").value;
+  removeExistingGifs();
+  fetchGifsBySearchTerm(searchTerm);
+};
+
+const removeExistingGifs = () => {
+  const giphySection = document.getElementsByClassName("giphy-container")[0];
+  while (giphySection.firstChild) {
+    giphySection.removeChild(giphySection.lastChild);
+  }
+};
+
+const fetchGifsBySearchTerm = (searchTerm) => {
+  fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=12`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      json.data
+        .map((gif) => gif.images.fixed_height.url)
+        .forEach((url) => {
+          const giphySection =
+            document.getElementsByClassName("giphy-container")[0];
+          const img = document.createElement("img");
+          img.src = url;
+          img.className = "giphy";
+          giphySection.appendChild(img);
+        });
+    })
+    .catch((error) => console.log(error));
+};
+
+fetchGifsBySearchTerm("cats");
